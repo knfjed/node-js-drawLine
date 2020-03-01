431e634ddc6b1ef47aeb5e01f79c3c94b26a9616
knfjed <knfjed@gmail.com>
2020-03-01 21:24:14 +0900

    implemented clear button

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 var canvas = document.getElementById("canvas");
+var btn = document.getElementById("btn");
 var ctx = canvas.getContext("2d");
 var mouseX, mouseY;
 var state = false;
+var color = 0;
 
 canvas.width = window.innerWidth - 2;
 canvas.height = window.innerHeight - 2;
+
+ctx.lineWidth = 50;
+ctx.lineCap = "round";
 
 // マウスをダウン（クリック）したときstateはtrue
 canvas.addEventListener("mousedown", e => {
@@ -19,13 +24,22 @@ canvas.addEventListener("mouseup", e => {
 });
 
 // マウスがfalseの時のみ、以下の内容を実行（if文）
+// １つずつカラーをカウントアップさせる
+// 線の色を指定（hsl：色相、彩度、輝度の３要素で表現する）
 // biginPathで線を書き始め、どの位置から書き始めるかmoveTo(座標)で指定する
 // lineToでx座標からy座標まで線を描くと指定
 // strokeで線を引っ張る
 // mouseX,mouseYの座標を更新し続ける
+
 canvas.addEventListener("mousemove", e => {
   console.log(e.clientX);
   if (!state) return;
+
+  var w = Math.floor(Math.random() * 51);
+
+  ctx.lineWidth = w;
+  color++;
+  ctx.strokeStyle = "hsl(" + color + ", 100%, 50%)";
   ctx.beginPath();
   ctx.moveTo(mouseX, mouseY);
   ctx.lineTo(e.clientX, e.clientY);
@@ -33,4 +47,9 @@ canvas.addEventListener("mousemove", e => {
 
   mouseX = e.clientX;
   mouseY = e.clientY;
+});
+
+// クリアボタン
+btn.addEventListener("click", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 });
